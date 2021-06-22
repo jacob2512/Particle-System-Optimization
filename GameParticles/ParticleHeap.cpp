0d90e15a0adc855d2dd30fb22711ed8e4e3d562e
Jacob Thomas <jacob2512thomas@gmail.com>
2021-06-22 14:48:24 -0700

    - replacing windows heap functions with custom allocations current average over first minute - 2.923593 ms

--- GameParticles/ParticleHeap.cpp
+++ GameParticles/ParticleHeap.cpp
@@ -2,33 +2,80 @@
 
 #include "CustomAssert.h"
 #include <new.h>
+#include <stdlib.h>
 
 // Marks a variable as unused (to avoid a compile warning)
 #define UNREFERENCED_VARIABLE(P) (void)(P)
 
+#define BLOCK_HEADER sizeof(bool) + sizeof(Block*)
+#define BLOCK_SIZE BLOCK_HEADER + sizeof(Particle)
+
+Block* ParticleHeap::FindBlock() const
+{
+  Block* block = heap_head->top_block;
+  Block* res = nullptr;
+
+  while (block && !block->m_Occupied) {
+    res = block;
+    block = block->m_NextBlockHeader;
+  }
+
+  ASSERT(res != nullptr);
+  return res;
+}
+
+Block* ParticleHeap::GetBlock(Particle* p) const
+{
+  return (Block*)(p - BLOCK_HEADER);
+}
+
 ParticleHeap::ParticleHeap()
 {
-  heap_ptr = HeapCreate(HEAP_NO_SERIALIZE, particle_size, max_heap_size);
-  ASSERT(heap_ptr != nullptr);
+  heap_head = (HeapHeader*)malloc(max_heap_size);
+  ASSERT(heap_head != nullptr);
+  heap_head->top_block = nullptr;
 }
 
 ParticleHeap::~ParticleHeap()
 {
-  HeapDestroy(heap_ptr);
+  if (heap_head != nullptr)
+  {
+    free(heap_head);
+  }
 }
 
 Particle* ParticleHeap::ParticleAlloc() const
 {
-  void* pp = HeapAlloc(heap_ptr, HEAP_NO_SERIALIZE, particle_size);
-  ASSERT(pp != nullptr);
-  Particle* ppr = new(pp) Particle;
-  return ppr;
+
+  Particle* new_particle = new(malloc(particle_size)) Particle;
+
+  Block* new_block = new(malloc(BLOCK_SIZE)) Block;
+
+  if (heap_head->top_block != nullptr)
+  {
+    new_block = FindBlock();
+    //new_block->m_Occupied = true;
+    //new_block->m_NextBlockHeader = nullptr;
+    //new_block->m_Particle = new_particle;
+  }
+  else
+  {
+    heap_head->top_block = new_block;
+  }
+
+  heap_head->m_HeapUsedSize += BLOCK_SIZE;
+
+  return new_particle;
+
 }
 
-void ParticleHeap::ParticleFree(Particle* ptr) const
+void ParticleHeap::ParticleFree(Particle* p) const
 {
-  ptr->~Particle();
-  bool ret = HeapFree(heap_ptr, HEAP_NO_SERIALIZE, ptr);
-  UNREFERENCED_VARIABLE(ret);
-  ASSERT(ret != 0);
+  p->~Particle();
+
+  //Block* block_to_free = GetBlock(p);
+  //block_to_free->m_Occupied = false;
+
+  heap_head->m_HeapUsedSize -= BLOCK_SIZE;
+  free(p);
 }
--- GameParticles/ParticleHeap.h
+++ GameParticles/ParticleHeap.h
@@ -2,18 +2,31 @@
 
 #include "Particle.h"
 
-#define WIN32_LEAN_AND_MEAN
-#define WIN32_EXTRA_LEAN
-#include <windows.h>
+struct Block
+{
+  bool m_Occupied;
+  Block* m_NextBlockHeader;
+  Particle* m_Particle;
+};
+
+struct HeapHeader
+{
+  Block* top_block;
+  size_t m_HeapUsedSize;
+};
 
 class ParticleHeap
 {
 public:
 
+  Block* FindBlock() const; 
+  Block* GetBlock(Particle* p) const;
+
   ParticleHeap();
   ~ParticleHeap();
   Particle* ParticleAlloc() const;
   void ParticleFree(Particle* ptr) const;
+
   constexpr int GetMaxPatricles() { return max_particles - 1; };
 
 private:
@@ -21,5 +34,6 @@ private:
   static constexpr int max_particles = 30 * 1000;
   static constexpr int heap_overhead = 1024 * 512;
   static constexpr int max_heap_size = max_particles * particle_size + heap_overhead;
-  HANDLE heap_ptr;
+
+  HeapHeader* heap_head;
 };
